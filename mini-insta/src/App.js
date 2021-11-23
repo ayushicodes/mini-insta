659c178cbe9f7d112db36d1605dfed54a82d2fc2
ayushicodes <raiayushi12911@gmail.com>
2021-11-23 14:29:30 +0530

    logo updated

---
+++ mini-insta/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img src="	https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
