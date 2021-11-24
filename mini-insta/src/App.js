8a0df2ea9583377f652834c37986d2b08a31c149
ayushicodes <raiayushi12911@gmail.com>
2021-11-24 09:56:26 +0530

    post updated

--- mini-insta/src/App.js
+++ mini-insta/src/App.js
@@ -12,9 +12,9 @@ function App() {
           src="	https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt="logo" />
       </div>
-      <Post />
-      <Post />
-      <Post />
+      <Post username="ayushi_rai" caption="Hiii I am awesome" imageUrl="https://www.best4geeks.com/wp-content/uploads/2018/08/48-lovely-cute-girl-picture-1024x816.jpg" />
+      <Post username="cm_champ" caption="I love ayushi" imageUrl="https://i.stack.imgur.com/7pHXK.jpg?s=256&g=1" />
+
     </div>
   );
 }
--- mini-insta/src/Post.js
+++ mini-insta/src/Post.js
@@ -1,25 +1,25 @@
 import React from "react"
 import './post.css';
 import { Avatar } from "@material-ui/core";
-function Post() {
+function Post({ username, caption, imageUrl }) {
     return (
         <div className="post">
             <div className="post__header">
                 <Avatar
                     className="post__Avatar"
-                    alt="Remy Sharp"
+                    alt={username}
                     src="/broken-image.jpg"
                 />
-                <h3>username</h3>
+                <h3>{username}</h3>
             </div>
 
             <img
                 className="post__image"
-                src="https://th.bing.com/th/id/OIP.IaFMwJ2SIJ4d4Sl_5ekM2AAAAA?w=168&h=180&c=7&r=0&o=5&pid=1.7"
+                src={imageUrl}
                 alt="post_img" />
 
 
-            <h5 className="post__text"> <strong>ayuc_rai</strong>  I am feeling awesome</h5>
+            <h5 className="post__text"> <strong>{username}:</strong> {caption}</h5>
         </div>
     )
 }
